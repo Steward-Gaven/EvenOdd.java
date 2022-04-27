e264d6ef78282e9aa8b69f98917150ededd0cce9
Steward-Gaven <Stewarwn@mail.uc.edu>
2022-04-27 10:39:28 -0400

    Initial commit

---
+++ src/EvenOdd.java
@@ -0,0 +1,12 @@
+class Main {
+
+    public static void main(String[] args) {
+
+        int numToExamine = 5;
+        int moduloOfNum;
+        moduloOfNum = numToExamine % 2;
+
+        System.out.println("The remainder of the given number, divided by two is: " + moduloOfNum);
+        System.out.println(moduloOfNum);
+    }
+}
